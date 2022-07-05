887d015301825bff1a16d98277d773b799753aff
Willian Silvestre <70384084+WillianSi@users.noreply.github.com>
2022-07-05 20:32:30 -0300

    vs1

---
+++ src/main/java/br/edu/ifsuldeminas/mch/si/webii/postmanager/spring/model/Cidade.java
@@ -0,0 +1,36 @@
+package br.edu.ifsuldeminas.mch.si.webii.postmanager.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cidade {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	private String name;
+	private String state;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+}
